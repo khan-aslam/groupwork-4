adc204a2e668d8ca034e089baf059be2d9b086ea
khan-aslam <98106871+khan-aslam@users.noreply.github.com>
2022-03-10 19:50:41 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+
+$('.owl-carousel').owlCarousel({
+	loop: true,
+	margin: 10,
+	autoplay:true,
+	autoplayTimeout:1000,
+	autoplayHoverPause:true,
+	nav: true,
+	responsive: {
+		0: {
+			items: 1
+		},
+		600: {
+			items: 3
+		},
+		1000: {
+			items: 3
+		}
+	}
+})
